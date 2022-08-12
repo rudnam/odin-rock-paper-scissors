68d701d418a4f7e548dfaeb43f768978a96f4bed
rudnam <francisalbarracin2@gmail.com>
2022-08-12 18:27:26 +0800

    Add UI

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let playerScore = 0;
+let computerScore = 0;
+let gameOngoing = false;
+
+const pscore = document.querySelector('#pscore');
+const cscore = document.querySelector('#cscore');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const response = document.querySelector('#response');
+const start = document.querySelector('#start');
+
 function getComputerChoice() {
     const randomNum = Math.floor(Math.random() * 3);
     switch(randomNum) {
@@ -11,69 +23,80 @@ function getComputerChoice() {
             return 'Scissors';
             break;
         default:
-            console.log("invalid number")
+            console.log("invalid number");
             return;
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    a = playerSelection.toUpperCase();
-    b = computerSelection.toUpperCase();
+function playRound(e) {
+    if (!gameOngoing) {
+        return;
+    }
+    a = e.currentTarget.id.toUpperCase();
+    b = getComputerChoice().toUpperCase();
     switch(a) {
         case "ROCK":
             if (b === "SCISSORS") {
-                return "You win! Rock beats Scissors!"
+                playerScore += 1;
+                response.textContent = "You win! Rock beats Scissors!";
+            } else if (b === "PAPER") {
+                computerScore += 1;
+                response.textContent = "You lose! Paper beats Rock!";
+            } else {
+                response.textContent = "It's a tie! Rock against Rock!";
             }
-            if (b === "PAPER") {
-                return "You lose! Paper beats Rock!"
-            }
-            return "It's a tie! Rock against Rock!"
             break;
         case "PAPER":
             if (b === "ROCK") {
-                return "You win! Paper beats Rock!"
+                playerScore += 1;
+                response.textContent = "You win! Paper beats Rock!";
+            } else if (b === "SCISSORS") {
+                computerScore += 1;
+                response.textContent = "You lose! Scissors beats Paper!";
+            } else {
+                response.textContent = "It's a tie! Paper against Paper!";
             }
-            if (b === "SCISSORS") {
-                return "You lose! Scissors beats Paper!"
-            }
-            return "It's a tie! Paper against Paper!"
             break;
         case "SCISSORS":
             if (b === "PAPER") {
-                return "You win! Scissors beats Paper!"
+                playerScore += 1;
+                response.textContent = "You win! Scissors beats Paper!";
+            } else if (b === "ROCK") {
+                computerScore += 1;
+                response.textContent = "You lose! Rock beats Scissors!";
+            } else {
+                response.textContent = "It's a tie! Scissors against Scissors!";
             }
-            if (b === "ROCK") {
-                return "You lose! Rock beats Scissors!"
-            }
-            return "It's a tie! Scissors against Scissors!"
             break;
         default:
-            return "invalid input"
+            response.textContent = "invalid input";
     }
-}
-
-function game() {
-    let userScore = 0;
-    let computerScore = 0;
-    for (let i=0; i < 5; i++) {
-        userInput = prompt("Rock, Paper, or Scissors?");
-        roundResult = playRound(userInput, getComputerChoice());
-        roundResult.includes("win")? userScore += 1 : roundResult.includes("lose")? computerScore += 1 : null;
-        console.log(roundResult);
+    pscore.textContent = playerScore;
+    cscore.textContent = computerScore;
+    if (playerScore == 5) {
+        response.textContent = "Congratulations! You won!";
+        gameOngoing = false;
+        start.style.display = 'inline-block';
+    } else if (computerScore == 5) {
+        response.textContent = "Game over! You lose!";
+        gameOngoing = false;
+        start.style.display = 'inline-block';
     }
-    if (userScore > computerScore) {
-        gameResult = `You win! The scores are:
-        User: ${userScore}
-        Computer: ${computerScore}`;
-    } else if (computerScore > userScore) {
-        gameResult = `You lose! The scores are:
-        User: ${userScore}
-        Computer: ${computerScore}`;
-    } else {
-        gameResult = `It's a tie! The scores are:
-        User: ${userScore}
-        Computer: ${computerScore}`;
-    }
-    console.log(gameResult);
     return;
 }
+
+function startGame(e) {
+    gameOngoing = true;
+    e.currentTarget.style.display = 'none';
+    playerScore = 0;
+    computerScore = 0;
+    pscore.textContent = playerScore;
+    cscore.textContent = computerScore;
+    response.textContent = "Choose your weapon";
+    return;
+}
+
+start.addEventListener('click', startGame);
+rock.addEventListener('click', playRound);
+paper.addEventListener('click', playRound);
+scissors.addEventListener('click', playRound);
